1bbbcedb9dc63a88d3748517ca483fdb79485578
Lakshya Birla <Lakshyabirla7cr@gmail.com>
2024-02-03 17:26:27 -0600

    find us section added

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-
+import { FooterOverlay, Newsletter } from '../../components';
+import { FiFacebook, FiInstagram, FiTwitter } from 'react-icons/fi';
 import './Footer.css';
 
-const Footer = () => (
-  <div>
-    Footer
-  </div>
-);
+const Footer = () => <div>Footer</div>;
 
 export default Footer;
